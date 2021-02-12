2659f907ff0b11ffbeeac131f2268e9e8fafe9a7
OctavioBartah <48717791+OctavioBartah@users.noreply.github.com>
2021-02-12 00:29:27 -0300

    Update help.js

--- Fxc7/help.js
+++ Fxc7/help.js
@@ -2,10 +2,6 @@ const help = (prefix, instagram, yt, name, pushname2, user, limitt, uptime, jam,
 	return `
 	
 
-\`\`\`INSTAGRAM OFFICIAL\`\`\`
-https://www.instagram.com/lordescreamocanal/
-
-
 \`\`\`POR FAVOR, LEIA AS NOTAS ABAIXO ANTES\`\`\`
 \`\`\`USANDO O BOT\`\`\`
 
@@ -17,7 +13,7 @@ https://www.instagram.com/lordescreamocanal/
 ┣⊱  \`\`\`ATIVO:\`\`\` ${kyun(uptime)}
 ┣⊱  \`\`\`HORA:\`\`\` *${jam} WIB*
 ┣⊱  \`\`\`ENCONTRO:\`\`\` *${tanggal}*
-┣⊱  \`\`\`VERSAO:\`\`\` *6.5.0*
+┣⊱  \`\`\`VERSAO:\`\`\` *1.0.0*
 ┣⊱  \`\`\`USUARIO REGISTRADO:\`\`\` *${user.length} User*
 ┣⊱  ❌ *SPAM*
 ┣⊱  ❌ *CALL & VC*
@@ -47,60 +43,24 @@ https://www.instagram.com/lordescreamocanal/
 ͏͏͏͏͏͏͏͏͏͏͏͏͏͏╭──────「 *MEDIA DOWNLOAD* 」
 ┴
 │➻ *${prefix}tiktokstalk username*
-│➻ *${prefix}igstalk _LORDE SCREAMO*
 │➻ *${prefix}insta Link*
 │➻ *${prefix}instastory username*
-│➻ *${prefix}ssweb url*
-│➻ *${prefix}url2img Url*
-│➻ *${prefix}tiktok*
-│➻ *${prefix}fototiktok*
 │➻ *${prefix}meme*
 │➻ *${prefix}memeindo*
 │➻ *${prefix}kbbi*
 │➻ *${prefix}wait*
 │➻ *${prefix}trendtwit*
-│➻ *${prefix}google berita terkini*
 ┬
 ╰────────────────────────
 
 
 ╭──────「 *CRIADOR MENU* 」
 ┴
-│➻ *${prefix}quotemaker tx/wtrmk/tema*
-│➻ *${prefix}nulis nama/kelas/text*
-│➻ *${prefix}rain reply image*
-│➻ *${prefix}trigger reply image*
-│➻ *${prefix}rip reply image*
-│➻ *${prefix}wasted reply image*
-│➻ *${prefix}cphlogo CAFE/BOT*
-│➻ *${prefix}cglitch SCREAMO/BOT*
-│➻ *${prefix}cpubg SCREAMO/BOT*
-│➻ *${prefix}cml SCREAMO*
-│
-│➻ *${prefix}tahta SCREAMO*
-│➻ *${prefix}croman SCREAMO dan BOT*
-│➻ *${prefix}cthunder SCREAMO*
-│➻ *${prefix}cbpink SCREAMO*
-│➻ *${prefix}cmwolf SCREAMO*
-│➻ *${prefix}csky SCREAMO*
-│➻ *${prefix}cwooden SCREAMO*
-│➻ *${prefix}cflower SCREAMO*
-│➻ *${prefix}clove SCREAMO*
-│➻ *${prefix}ccrossfire SCREAMO*
-│➻ *${prefix}cnaruto SCREAMO*
-│➻ *${prefix}cparty SCREAMO*
-│➻ *${prefix}cshadow SCREAMO*
-│➻ *${prefix}cminion SCREAMO*
 │➻ *${prefix}cneon CAFE*
 │➻ *${prefix}cneon2 CAFE*
 │➻ *${prefix}cneongreen CAFE*
 │➻ *${prefix}c3d CAFE*
 │➻ *${prefix}csky CAFE*
-│➻ *${prefix}tts id Haii*
-│➻ *${prefix}ttp Screamo*
-│➻ *${prefix}cballon Screamo*
-│➻ *${prefix}cpaper Screamo*
-│➻ *${prefix}slide Screamo BOT WA*
 │
 │➻ *${prefix}stiker*
 │➻ *${prefix}gifstiker*
@@ -133,6 +93,7 @@ https://www.instagram.com/lordescreamocanal/
 │➻ *${prefix}animecry*
 │➻ *${prefix}neonime*
 │➻ *${prefix}animekiss*
+│➻ *${prefix}animewp*
 │➻ *${prefix}wink*
 ┬
 ╰───────────────────────
@@ -166,87 +127,12 @@ https://www.instagram.com/lordescreamocanal/
 ╭────────────────────────
 ┴
 │➻ *${prefix}nsfw On/Off*
-│➻ *${prefix}nsfwloli*
-│➻ *${prefix}nsfwblowjob*
-│➻ *${prefix}nsfwneko*
-│➻ *${prefix}nsfwtrap*
-│➻ *${prefix}hentai* ja sabe aqui e foto doida 
+│➻ *${prefix}loli*
+│➻ *${prefix}blowjob*
+│➻ *${prefix}neko*
+│➻ *${prefix}trap*
+│➻ *${prefix}hentai*
 │➻ *${prefix}simih On/Off*
-┬
-╰────────────────────────
-
-
-╭──────「 *OTHERS FUN & GAME* 」
-┴
-│➻ *${prefix}anjing*
-│➻ *${prefix}kucing*
-│➻ *${prefix}testime*
-│➻ *${prefix}hilih*
-│➻ *${prefix}say*
-│➻ *${prefix}apakah*
-│➻ *${prefix}kapankah*
-│➻ *${prefix}bisakah*
-│➻ *${prefix}rate*
-│➻ *${prefix}watak*
-│➻ *${prefix}hobby*
-│➻ *${prefix}infogempa*
-│➻ *${prefix}infonomor*
-│➻ *${prefix}quotes*
-│➻ *${prefix}truth*
-│➻ *${prefix}dare*
-│➻ *${prefix}katabijak*
-│➻ *${prefix}fakta*
-│➻ *${prefix}darkjokes*
-│➻ *${prefix}bucin*
-│➻ *${prefix}pantun*
-│➻ *${prefix}katacinta*
-│➻ *${prefix}jadwaltvnow*
-│➻ *${prefix}hekerbucin*
-│➻ *${prefix}katailham*
-│➻ *${prefix}animewp*
-┬
-╰────────────────────────
-
-╭──────────────────────────
-┴
-│➻ *${prefix}jarak Banyuwangi/Surabaya*
-│➻ *${prefix}translate en/Apa kabar?*
-│➻ *${prefix}pasangan Farhan/Iriene*
-│➻ *${prefix}gantengcek Farhan*
-│➻ *${prefix}cantikcek Iriene*
-│➻ *${prefix}artinama Farhan*
-│➻ *${prefix}persengay Topan*
-│➻ *${prefix}pbucin Farhan*
-│➻ *${prefix}bpfont Farhan*
-│➻ *${prefix}textstyle SCREAMO*
-│➻ *${prefix}jadwaltv antv*
-│➻ *${prefix}lirik melukis senja*
-│➻ *${prefix}chord Melukis senja*
-│➻ *${prefix}wiki Adolf Hitler*
-│➻ *${prefix}brainly pertanyaan*
-│➻ *${prefix}resepmasakan rawon*
-│➻ *${prefix}map Banyuwangi*
-│➻ *${prefix}film Fast and Farious*
-│➻ *${prefix}pinterest gambar kucing*
-│➻ *${prefix}infocuaca Banyuwangi*
-│➻ *${prefix}jamdunia Banyuwangi*
-│➻ *${prefix}mimpi Ular*
-│➻ *${prefix}infoalamat jalan Banyuwangi*
-│➻ *${prefix}playstore WhatsApp*
-┬
-╰───────────────────────────
-
-
-╭────────────────────────
-┴
-│➻ *${prefix}asupan*
-│➻ *${prefix}tebakgambar*
-│➻ *${prefix}caklontong*
-│➻ *${prefix}family100*
-│➻ *${prefix}kalkulator 13*12*
-│➻ *${prefix}wp gunung*
-│➻ *${prefix}moddroid lightroom*
-│➻ *${prefix}happymod lightroom*
 ┬
 ╰────────────────────────
 
@@ -255,7 +141,7 @@ https://www.instagram.com/lordescreamocanal/
 │➻ *${prefix}cerpen*
 │➻ *${prefix}cersex*
 │➻ *${prefix}xxx japan*
-│➻ *${prefix}pornhub stepMoms* pornozao kkk
+│➻ *${prefix}pornhub stepMoms* 
 ┬
 ╰────────────────────────
 
@@ -265,33 +151,6 @@ https://www.instagram.com/lordescreamocanal/
 │➻ *${prefix}quran*
 │➻ *${prefix}quransurah 1*
 │➻ *${prefix}tafsir kafir*
-┬
-╰────────────────────────
-
-╭────────────────────────
-┴
-│➻ *${prefix}becrypt string*
-│➻ *${prefix}encode64 string*
-│➻ *${prefix}decode64 encrypt*
-│➻ *${prefix}encode32 string*
-│➻ *${prefix}decode32 encrypt*
-│➻ *${prefix}encbinary string*
-│➻ *${prefix}decbinary encrypt*
-│➻ *${prefix}encoctal string*
-│➻ *${prefix}decoctal encrypt*
-│➻ *${prefix}hashidentifier Encrypt Hash*
-│➻ *${prefix}dorking dork*
-│➻ *${prefix}pastebin teks*
-│➻ *${prefix}tinyurl link*
-│➻ *${prefix}bitly link*
-┬
-╰────────────────────────
-
-╭────────────────────────
-┴
-│➻ *${prefix}spamcall 083xxxxxxxxx*
-│➻ *${prefix}spamsms 083xxxxxxxx/jumlah*
-│➻ *${prefix}spamgmail lordescreamo@gmail.com*
 ┬
 ╰────────────────────────
 
@@ -319,28 +178,6 @@ https://www.instagram.com/lordescreamocanal/
 ┬
 ╰────────────────────────
 
-
-╭────────「 *PREMIUM APENAS* 」
-┴
-│➻ *${prefix}playmp3 menepi*
-│➻ *${prefix}fb link video*
-│➻ *${prefix}snack link snack video*
-│➻ *${prefix}ytmp3 link yt* ta baixando musicas youTube
-│➻ *${prefix}ytmp4 link yt*
-│➻ *${prefix}joox The Ultimate Monologue*
-│➻ *${prefix}smule Link Video Smule*
-┬
-╰────────────────────────
-
-
-╭─────「 *CANAIS PARCEIROS${name}* 」
-│
-├➲ *COPIADOR UCHIHA*
-├➲ *MAFIA DOS TUTORS*
-├➲ *PROFESSIONNEL JOKER*
-├➲ *CORTES SCREAMO*
-├➲ *BOT LORDE SCREAMO*
-╰────────────────────────`
 }
 
 exports.help = help
