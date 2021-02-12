4e47dcb0ea20d6a10f398517b483cc7a57a2628f
OctavioBartah <48717791+OctavioBartah@users.noreply.github.com>
2021-02-12 01:24:11 -0300

    Update help.js

--- Fxc7/help.js
+++ Fxc7/help.js
@@ -5,133 +5,136 @@ const help = (prefix, instagram, yt, name, pushname2, user, limitt, uptime, jam,
 \`\`\`POR FAVOR, LEIA AS NOTAS ABAIXO ANTES\`\`\`
 \`\`\`USANDO O BOT\`\`\`
 
+❉❉❉ LOLI_SENPAI ❉❉❉
+❉       
+❉         ❉ Menu ❉
+❉
+❉  ╭─❉ *SOBRE ${name}* ❉──────
+❉  ┴
+❉  │➻ *${prefix}report lapor bug* primeira git termux com antilink
+❉  │➻ *${prefix}info*
+❉  │➻ *${prefix}donasi* mendingacao
+❉  │➻ *${prefix}owner*
+❉  │➻ *${prefix}speed*
+❉  │➻ *${prefix}daftar*
+❉  │➻ *${prefix}totaluser*
+❉  │➻ *${prefix}grouplist*
+❉  │➻ *${prefix}blocklist*
+❉  │➻ *${prefix}banlist*
+❉  │➻ *${prefix}premiumlist*
+❉  ┬
+❉  ╰────────────────────────
+❉
+❉
+❉
+❉  ╭─❉ *MÍDIA DOWNLOAD* ❉──────
+❉  |
+❉  |➻ ${prefix}meme
+❉  |➻ ${prefix}memeindo
+❉  |➻ ${prefix}wait
+❉  |➻ ${prefix}ytmp4
+❉  |➻ ${prefix}ytmp3
+❉  ╰───────────────────────
+❉
+❉  ╭─❉ *CRIADOR MÍDIA* ❉──────  
+❉  ┴
+❉  │➻ *${prefix}harrypotter CAFE*
+❉  │➻ *${prefix}stiker*
+❉  │➻ *${prefix}gifstiker*
+❉  │➻ *${prefix}toimg*
+❉  │➻ *${prefix}img2url*
+❉  │➻ *${prefix}nobg*
+❉  │➻ *${prefix}tomp3*
+❉  │➻ *${prefix}ocr*
+❉  ┬
+❉  ╰──────────────────────────
+❉
+❉
+❉  ╭─❉ *GRUPO APENAS* ❉ 
+❉  ┴
+❉  │➻ *${prefix}modeanime On/Off*
+❉  │➻ *${prefix}naruto*
+❉  │➻ *${prefix}minato*
+❉  │➻ *${prefix}boruto*
+❉  │➻ *${prefix}hinata*
+❉  │➻ *${prefix}sakura*
+❉  │➻ *${prefix}sasuke*
+❉  │➻ *${prefix}kaneki*
+❉  │➻ *${prefix}toukachan*
+❉  │➻ *${prefix}rize*
+❉  │➻ *${prefix}akira*
+❉  │➻ *${prefix}itori*
+❉  │➻ *${prefix}kurumi*
+❉  │➻ *${prefix}miku*
+❉  │➻ *${prefix}anime*
+❉  │➻ *${prefix}animecry*
+❉  │➻ *${prefix}neonime*
+❉  │➻ *${prefix}animekiss*
+❉  │➻ *${prefix}animewp*
+❉  │➻ *${prefix}wink*
+❉  ┬
+❉  ╰───────────────────────
 
-
-╭──────「 *SOBRE ${name}* 」
-┴
-│➻ *${prefix}report lapor bug* primeira git termux com antilink
-│➻ *${prefix}info*
-│➻ *${prefix}donasi* mendingacao
-│➻ *${prefix}owner*
-│➻ *${prefix}speed*
-│➻ *${prefix}daftar*
-│➻ *${prefix}totaluser*
-│➻ *${prefix}grouplist*
-│➻ *${prefix}blocklist*
-│➻ *${prefix}banlist*
-│➻ *${prefix}premiumlist*
-┬
-╰────────────────────────
-
-
-͏͏͏͏͏͏͏͏͏͏͏͏͏͏╭──────「 *MEDIA DOWNLOAD* 」
-┴
-│➻ *${prefix}meme*
-│➻ *${prefix}memeindo*
-│➻ *${prefix}wait*
-┬
-╰────────────────────────
-
-
-╭──────「 *CRIADOR MENU* 」
-┴
-│➻ *${prefix}harrypotter CAFE*
-│➻ *${prefix}stiker*
-│➻ *${prefix}gifstiker*
-│➻ *${prefix}toimg*
-│➻ *${prefix}img2url*
-│➻ *${prefix}nobg*
-│➻ *${prefix}tomp3*
-│➻ *${prefix}ocr*
-┬
-╰──────────────────────────
-
-
-╭───────「 *GRUPO APENAS* 」
-┴
-│➻ *${prefix}modeanime On/Off*
-│➻ *${prefix}naruto*
-│➻ *${prefix}minato*
-│➻ *${prefix}boruto*
-│➻ *${prefix}hinata*
-│➻ *${prefix}sakura*
-│➻ *${prefix}sasuke*
-│➻ *${prefix}kaneki*
-│➻ *${prefix}toukachan*
-│➻ *${prefix}rize*
-│➻ *${prefix}akira*
-│➻ *${prefix}itori*
-│➻ *${prefix}kurumi*
-│➻ *${prefix}miku*
-│➻ *${prefix}anime*
-│➻ *${prefix}animecry*
-│➻ *${prefix}neonime*
-│➻ *${prefix}animekiss*
-│➻ *${prefix}animewp*
-│➻ *${prefix}wink*
-┬
-╰───────────────────────
-
-╭────────────────────────
-┴
-│➻ *${prefix}welcome On/Off*
-│➻ *${prefix}grup buka/tutup*
-│➻ *${prefix}antilink on/off*
-│➻ *${prefix}ownergrup*
-│➻ *${prefix}setpp*
-│➻ *${prefix}infogc*
-│➻ *${prefix}add*
-│➻ *${prefix}kick*
-│➻ *${prefix}promote*
-│➻ *${prefix}demote*
-│➻ *${prefix}setname* muda nome grupo 
-│➻ *${prefix}setdesc* altera descricao
-│➻ *${prefix}linkgrup*
-│➻ *${prefix}tagme*
-│➻ *${prefix}hidetag* marcar invisivel
-│➻ *${prefix}tagall*
-│➻ *${prefix}mentionall* menciona membro
-│➻ *${prefix}fitnah*
-│➻ *${prefix}listadmin*
-│➻ *${prefix}openanime*
-│➻ *${prefix}edotense*
-┬
-╰────────────────────────
-
-╭────────────────────────
-┴
-│➻ *${prefix}nsfw On/Off*
-│➻ *${prefix}loli*
-│➻ *${prefix}blowjob*
-│➻ *${prefix}neko*
-│➻ *${prefix}trap*
-│➻ *${prefix}hentai*
-│➻ *${prefix}simih On/Off*
-┬
-╰────────────────────────
-
-╭─────────「 *SO PROPRIETARIO* 」
-┴
-│➻ *${prefix}addprem mentioned*
-│➻ *${prefix}removeprem mention*
-│➻ *${prefix}setmemlimit*
-│➻ *${prefix}setreply*
-│➻ *${prefix}setprefix*
-│➻ *${prefix}setnamebot* muda nome desse bot
-│➻ *${prefix}setppbot*
-│➻ *${prefix}bc*
-│➻ *${prefix}bcgc*
-│➻ *${prefix}ban*
-│➻ *${prefix}unban*
-│➻ *${prefix}block*
-│➻ *${prefix}unblock*
-│➻ *${prefix}clearall*
-│➻ *${prefix}delete*
-│➻ *${prefix}clone*
-│➻ *${prefix}getses*
-│➻ *${prefix}leave*
-┬
-╰────────────────────────
+❉  ╭────────────────────────
+❉  ┴
+❉  │➻ *${prefix}welcome On/Off*
+❉  │➻ *${prefix}grup buka/tutup*
+❉  │➻ *${prefix}antilink on/off*
+❉  │➻ *${prefix}ownergrup*
+❉  │➻ *${prefix}setpp*
+❉  │➻ *${prefix}infogc*
+❉  │➻ *${prefix}add*
+❉  │➻ *${prefix}kick*
+❉  │➻ *${prefix}promote*
+❉  │➻ *${prefix}demote*
+❉  │➻ *${prefix}setname* muda nome grupo 
+❉  │➻ *${prefix}setdesc* altera descricao
+❉  │➻ *${prefix}linkgrup*
+❉  │➻ *${prefix}tagme*
+❉  │➻ *${prefix}hidetag* marcar invisivel
+❉  │➻ *${prefix}tagall*
+❉  │➻ *${prefix}mentionall* menciona membro
+❉  │➻ *${prefix}fitnah*
+❉  │➻ *${prefix}listadmin*
+❉  │➻ *${prefix}openanime*
+❉  │➻ *${prefix}edotense*
+❉  ┬
+❉  ╰────────────────────────
+❉
+❉  ╭────────────────────────
+❉  ┴
+❉  │➻ *${prefix}nsfw On/Off*
+❉  │➻ *${prefix}loli*
+❉  │➻ *${prefix}blowjob*
+❉  │➻ *${prefix}neko*
+❉  │➻ *${prefix}trap*
+❉  │➻ *${prefix}hentai*
+❉  │➻ *${prefix}simih On/Off*
+❉  ┬
+❉  ╰────────────────────────
+❉
+❉  ╭─❉ *SO PROPRIETARIO* ❉ 
+❉  ┴
+❉  │➻ *${prefix}addprem mentioned*
+❉  │➻ *${prefix}removeprem mention*
+❉  │➻ *${prefix}setmemlimit*
+❉  │➻ *${prefix}setreply*
+❉  │➻ *${prefix}setprefix*
+❉  │➻ *${prefix}setnamebot* muda nome desse bot
+❉  │➻ *${prefix}setppbot*
+❉  │➻ *${prefix}bc*
+❉  │➻ *${prefix}bcgc*
+❉  │➻ *${prefix}ban*
+❉  │➻ *${prefix}unban*
+❉  │➻ *${prefix}block*
+❉  │➻ *${prefix}unblock*
+❉  │➻ *${prefix}clearall*
+❉  │➻ *${prefix}delete*
+❉  │➻ *${prefix}clone*
+❉  │➻ *${prefix}getses*
+❉  │➻ *${prefix}leave*
+❉  ┬
+❉  ╰────────────────────────
 
 }
 
